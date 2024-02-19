d86d17f0970b9e4332d7823cb0166a03c4b3fe5a
ZabdielV <premiunn@hotmail.com>
2024-02-18 21:18:44 -0600

    fix: references error

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,6 +6,6 @@ echo "** Running commands inside docker image, not vagrant ***********"
 echo "** When running this commands, jenkins already pull the last git code from remote ***********"
 echo "***************************"
 
-WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/home/Jenkins-Pipeline-Docker-Project/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
